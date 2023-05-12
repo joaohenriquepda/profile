2fc947282a7432796f54a6bc58d34d6cdf3f84c0
João Henrique <joaohenrique.p.almeida@gmail.com>
2023-05-12 17:55:12 +0100

    Add url query get pokemon information

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const API_URL = 'https://pokeapi.co/api/v2/pokemon/ditto';
+
+  const [data, setData] = useState<any>(null); // Change 'any' to the expected data type
+
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const jsonData = await response.json();
+      setData(jsonData);
+      console.log(jsonData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+
+  useEffect(() => {
+    console.log("AQUI");
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +34,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
